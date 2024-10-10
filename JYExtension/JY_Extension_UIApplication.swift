1b8c55346cdef177a10505ff3e0795c1de32999e
JYYQLin <92315746+JYYQLin@users.noreply.github.com>
2024-10-10 15:34:26 +0800

    Update JY_Extension_UIApplication.swift

--- JYExtension/JY_Extension_UIApplication.swift
+++ JYExtension/JY_Extension_UIApplication.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Photos
 
 extension UIApplication {
     /// 获取当前BundleID
@@ -31,6 +32,46 @@ extension UIApplication {
         DispatchQueue.main.async {
             if UIApplication.shared.canOpenURL(url) == true {
                 UIApplication.shared.open(url, options: [:], completionHandler: completionHandler)
+            }
+        }
+    }
+}
+
+extension UIApplication {
+
+    /** 检测相册授权情况 */
+    public static func yq_request_photo_authorization(_ handler: @escaping (_ isAuthorized: Bool, _ status: PHAuthorizationStatus) -> Void) {
+        
+        PHPhotoLibrary.requestAuthorization { status in
+            if status == .notDetermined || status == .restricted || status == .denied{
+                //  用户尚未作出关于这个应用的选择
+                
+                //  该应用程序未被授权访问照片库，用户也无法授予这样的权限。
+                    // 比如家长控制。
+                
+                // 用户已明确拒绝此应用程序访问照片数据
+                DispatchQueue.main.async {
+                    handler(false, status)
+                }
+            }else if status == .authorized {
+                // 用户已授权此应用程序访问照片数据
+                DispatchQueue.main.async {
+                    handler(true, status)
+                }
+            }else {
+                if #available(iOS 14, *) {
+                    if status == .limited {
+                        // 用户授权此应用程序进行有限的照片库访问
+                        DispatchQueue.main.async {
+                            handler(true, status)
+                        }
+                    }
+                } else {
+                    //  未知, 按照未授权处理
+                    DispatchQueue.main.async {
+                        handler(false, status)
+                    }
+                }
             }
         }
     }
